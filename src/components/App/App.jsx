c518a3d41593c915914dc5d34857ccdffdd9d96d
Serhii Yatsenko <serhii2111@yahoo.com>
2026-06-14 22:55:59 +0300

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,30 @@
+import Profile from "../Profile/Profile";
+import userData from '../../data/userData.json';
+
+import FriendList from '../FriendList/FriendList';
+import friends from '../../data/friends.json';
+
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+import transactions from '../../data/transactions.json';
+import '../../../node_modules/modern-normalize/modern-normalize.css';
+
+
+
+const App = () => {
+    return (
+        <div>
+            <Profile
+                name={userData.username}
+                tag={userData.tag}
+                location={userData.location}
+                image={userData.avatar}
+                stats={userData.stats}
+            />
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
+        </div>
+    );
+};
+
+
+export default App;
