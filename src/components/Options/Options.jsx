d13a11d740bc13cd8db2db421b7b0bc376d71472
Serhii Yatsenko <serhii2111@yahoo.com>
2026-06-18 12:18:44 +0300

    Update Options.jsx

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -2,13 +2,13 @@ import style from "./Options.module.css";
 
 
 
-const Options = ({no, onResetR, onGood, onNeutral, onBad}) => {    
+const Options = ({noFeedback, onResetR, onGood, onNeutral, onBad}) => {    
     return (
         <div className={style.container}>
             <button className={style.button} onClick={onGood}>Good</button>
             <button className={style.button} onClick={onNeutral}>Neutral</button>
             <button className={style.button} onClick={onBad}>Bad</button>
-            {!no && <button className={style.button} onClick={onResetR}>Reset</button>}
+            {!noFeedback && <button className={style.button} onClick={onResetR}>Reset</button>}
         </div>
     );
 };
